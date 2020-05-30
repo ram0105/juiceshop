1b2ae8e60eef8dec3c8e423396ac185b4f8b201e
Ramprasanth Azhagappan <ramprasanth.a@thoughtworks.com>
2020-05-30 15:26:27 +0530

    Added Waits

--- src/test/java/com/juiceshop/JuiceShopTest.java
+++ src/test/java/com/juiceshop/JuiceShopTest.java
@@ -3,34 +3,45 @@ package com.juiceshop;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 public class JuiceShopTest {
     WebDriver webDriver;
+    String EMAIL = "test5.93@gmail.com";
+    String PASSWORD = "test@123";
+    String CHROME_DRIVER_PATH="/Users/ramprasanth/Downloads/chromedriver";
+    WebDriverWait wait;
+
+
     @BeforeClass
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver","/Users/ramprasanth/Downloads/chromedriver");
-        webDriver= new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_PATH);
+        webDriver = new ChromeDriver();
+        wait= new WebDriverWait(webDriver,20);
+        webDriver.manage().window().maximize();
+        webDriver.get("http://127.0.0.1:3000/#/register");
     }
 
     @Test
     public void accountCreation() {
-        webDriver.get("http://127.0.0.1:3000/#/register");
-        webDriver.findElement(By.xpath("//span[text()='Dismiss']")).click();
-        webDriver.findElement(By.id("emailControl")).sendKeys("test.93@gmail.com");
-        webDriver.findElement(By.id("passwordControl")).sendKeys("test@123");
-        webDriver.findElement(By.id("repeatPasswordControl")).sendKeys("test@123");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@aria-label='Close Welcome Banner']")));
+        webDriver.findElement(By.xpath("//button[@aria-label='Close Welcome Banner']")).click();
+        webDriver.findElement(By.id("emailControl")).sendKeys(EMAIL);
+        webDriver.findElement(By.id("passwordControl")).sendKeys(PASSWORD);
+        webDriver.findElement(By.id("repeatPasswordControl")).sendKeys(PASSWORD);
         webDriver.findElement(By.xpath("//mat-select[@name='securityQuestion']")).click();
         webDriver.findElement(By.xpath("//span[(@class='mat-option-text') and text()[normalize-space() = 'Your eldest siblings middle name?']]")).click();
-        webDriver.findElement(By.id("securityAnswerControl")).sendKeys("test");
+        webDriver.findElement(By.id("securityAnswerControl")).sendKeys(PASSWORD);
         webDriver.findElement(By.id("registerButton")).click();
     }
 
     @Test
     public void login() {
-        webDriver.findElement(By.id("email")).sendKeys("test.93@gmail.com");
-        webDriver.findElement(By.id("password")).sendKeys("test@123");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("email")));
+        webDriver.findElement(By.id("email")).sendKeys(EMAIL);
+        webDriver.findElement(By.id("password")).sendKeys(PASSWORD);
         webDriver.findElement(By.id("loginButton")).click();
     }
 }
